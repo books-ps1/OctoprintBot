33dfaadad474eb8675d0adda4be999c4a5e468da
Your Name <you@example.com>
2020-09-26 01:53:48 -0500

    Revise OctoprintBot to use MQTT instead of Octoprint API

--- lib/octoprint_bot.rb
+++ lib/octoprint_bot.rb
@@ -3,7 +3,7 @@
 #
 #https://github.com/slack-ruby/slack-ruby-bot
 require 'slack-ruby-bot'
-require_relative './octoprint_api_client'
+require 'paho-mqtt'
 
 #
 # Note that you have to set OCTOPRINT_API_KEY in the environment or you
@@ -12,39 +12,93 @@ require_relative './octoprint_api_client'
 #     export OCTOPRINT_API_KEY=1234567890ABCDEF1234567890ABCDEF
 #
 class OctoprintBot < SlackRubyBot::Bot
+  PS1_SERVER_URL = "10.10.1.224"
+  LOCALHOST_URL = "localhost"
+  SERVER_PORT = 1883
+
   PRINTERS = [
-    { name: "Prusa Blue", url: "http://10.30.0.11" },
-    { name: "Prusa Purple", url: "http://10.30.0.12" },
-    { name: "Prusa Red", url: "http://10.30.0.13/" },
-    { name: "Ender Yellow", url: "http://10.30.0.21" },
-    { name: "Ender Green", url: "http://10.30.0.22" }
+    { name: "Prusa Blue",   topic: "3dprinting/prusa/blue" },
+    { name: "Prusa Purple", topic: "3dprinting/prusa/purple" },
+    { name: "Prusa Red",    topic: "3dprinting/prusa/red" },
+    { name: "Ender Yellow", topic: "3dprinting/ender/yellow" },
+    { name: "Ender Green",  topic: "3dprinting/ender/green" }
   ]
+
+  def initialize
+    @client = PahoMqtt::Client.new
+    @message_counter = 0
+    @waiting_suback = true
+    @waiting_puback = true
+    @mqtt_url = LOCALHOST_URL
+    @mqtt_port = SERVER_PORT
+
+    @logger = Logger.new(STDOUT)
+    #@logger = Logger.new File.new('example.log', 'w')
+    #@logger.level = Logger::WARN
+    @logger.level = Logger::INFO
+    @mutex = Mutex.new
+
+    PRINTERS.each do |printer|
+      self.subscribe(printer[:topic])
+    end
+
+    @printers = PRINTERS.clone
+
+    @thread_mqtt = Thread.new { self.connect }
+
+    super
+  end
+
+  def connect
+    @client.on_message do |message|
+      @logger.info("Message recieved on topic: #{message.topic}")
+      @logger.info(">>> #{message.payload}")
+
+      printer_id = @printers.map { |p| p[:topic] }.index(message.topic)
+      @mutex.synchronize do
+        @printers[printer_id][:msg] = message.payload
+      end
+
+      @message_counter += 1
+    end
+
+    ### Register a callback on suback to assert the subcription
+    @client.on_suback do
+      @waiting_suback = false
+      @logger.info("Subscribed")
+    end
+
+    self.subscribe
+
+    ### Register a callback for puback event when receiving a puback
+    @client.on_puback do
+      @waiting_puback = false
+      @logger.info("Message Acknowledged")
+    end
+
+    ### Connect to the eclipse test server on port 1883 (Unencrypted mode)
+    @client.connect(@mqtt_url, @mqtt_port)
+  end
+
+  # Subscribe to the base topic.
+  # Not used
+  def subscribe(topic = '/3dprinting/prusa/blue')
+    @client.subscribe([topic, 2])
+
+    # Waiting for the suback answer and excute the previously set
+    # on_suback callback
+    while @waiting_suback do
+      sleep 0.001
+    end
+    @waiting_suback = true
+  end
 
   # Use the '!jobs' command in slack to get a list of active 3d printer jobs
   match /\A!jobs\z/ do |client, data, match|
-
     text_array = []
-    PRINTERS.each do |printer|
-      api_client = OctoprintApiClient.new(printer[:url])
 
-      job = api_client.job_info
-      job_progress = job['progress']
-
-      unless job_progress.nil?
-        completion = job_progress['completion']
-        time_left =  job_progress['printTimeLeft']
-      end
-
-      if time_left && completion && api_client.is_on?
-        time_str = Time.at(time_left).utc.strftime("%H:%M:%S")
-        msg = "%s: %s (%0.2f%%)" % [printer[:name], time_str, completion]
-        text_array.push msg
-
-      else
-        msg = "%s: is off" % printer[:name]
-        text_array.push msg
-
-      end
+    @mutex.synchronize do
+      text_array = @printers.map { |p| p[:msg] }
     end
 
     client.say(channel: data.channel, text: text_array.join("\n"))
